84f73b3be8603ab769e4bf03cd21059378f16a16
Toon Hasenack <Toon.Hasenack@outlook.com>
2023-06-12 21:09:23 +0200

    yep

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    publication_date = models.DateField()
